ff29aab077f310727cf1d5fa52c947604e209306
Thales Machado <thalesmmachado@gmail.com>
2023-12-20 17:24:15 -0300

    feat: implement the others methods needed on the repository interface

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -2,10 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 interface RepositoryInterface
 {
     public function all(): Collection;
+
+    public function create(array $data): Model;
+
+    public function update(array $data, User $user): bool;
+
+    public function delete(User $user): bool;
 }
