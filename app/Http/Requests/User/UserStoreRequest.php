4b5e04a0464db7b00353789f6498616ee20262d1
Thales Machado <thalesmmachado@gmail.com>
2023-12-20 17:26:29 -0300

    feat: form request to login and to create a user

---
+++ app/Http/Requests/User/UserStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255', 'min:3'],
+            'last_name' => ['required', 'string', 'max:255', 'min:3'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'mobile' => ['required', 'string', 'max:15', 'min:9'],
+            'username' => ['required', 'string', 'max:255', 'min:3'],
+        ];
+    }
+}
