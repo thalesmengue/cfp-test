d1db16091aee5baa921e92787c7261b4e72564e0
Thales Machado <thalesmmachado@gmail.com>
2023-12-20 13:22:00 -0300

    feat: do the layout for the app and the application navigation bar

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow mb-3">
+    <div class="container-fluid">
+        <a class="navbar-brand font-weight-bold text-gray-900" href="#">CFP Energy</a>
+        <div class="px-4"></div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-gray-900 border-bottom border-2 border-primary" href="#">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-gray-900 border-bottom border-2 border-primary" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
