906d77314feca5f55fb1cf64d9195ccd8a79b2fe
Thales Machado <thalesmmachado@gmail.com>
2023-12-20 22:45:06 -0300

    feat: set up jquery datatables and create index view to fetch all users and show them

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,10 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+
         <!-- Scripts -->
+        <link href="https://cdn.datatables.net/v/bs5/dt-1.13.8/r-2.5.0/datatables.min.css" rel="stylesheet">
+
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
@@ -22,5 +25,17 @@
                 @yield('content')
             </main>
         </div>
+        <script
+            src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/v/bs5/dt-1.13.8/r-2.5.0/datatables.min.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#users').DataTable({
+                    "responsive": true
+                });
+            });
+        </script>
     </body>
 </html>
