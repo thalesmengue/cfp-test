bcb6c0a9c65ef59940cb6ffa197c7c1256e07209
Thales Machado <thalesmmachado@gmail.com>
2023-12-21 00:18:43 -0300

    feat: function to create a user and view to create a new user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use App\Actions\Users\UserStoreAction;
 use App\Actions\Users\UserDeleteAction;
 use App\Actions\Users\UserFetchAllAction;
+use App\Http\Requests\User\UserStoreRequest;
 
 class UserController extends Controller
 {
@@ -24,14 +26,20 @@ class UserController extends Controller
         return view('users.create');
     }
 
-    public function store()
+    public function store(UserStoreRequest $request, UserStoreAction $action): RedirectResponse
     {
-        //
+        $data = $request->validated();
+
+        $action->execute($data);
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
     }
 
-    public function edit()
+    public function edit(User $user): View
     {
-        //
+        return view('users.edit', [
+            'user' => $user
+        ]);
     }
 
     public function update()
